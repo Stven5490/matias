98d816901f6fa1cdf610faa1e8cfe6cdaa4c244c
Stven5490 <est.caceres@gmail.com>
2024-12-19 11:16:04 -0300

    Fix compilado y ejecucion de programa

--- src/main/java/com/ecsa/divisas/application.java
+++ src/main/java/com/ecsa/divisas/application.java
@@ -3,9 +3,18 @@ package com.ecsa.divisas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 public class application {
     public static void main(String[] args) {
         SpringApplication.run(application.class, args);
+        try {
+            // Abre el navegador en la URL principal
+            Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+        } catch (Exception e) {
+            System.err.println("Error al abrir el navegador: " + e.getMessage());
+        }
     }
 }
